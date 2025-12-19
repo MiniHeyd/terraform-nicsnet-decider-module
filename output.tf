14c3e74a93b06944edb231c5f659551f3704e592
Steve Heydweiller <steve@highdwellercreative.com>
2025-12-19 09:41:48 -0600

    add version indicator output

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "decisions" {
 value = <<EOT
-          Decisions can be hard. You might even say fatiguing. 
+          Decisions can be hard. You might even say fatiguing.
           These are the ones I've made for you so you don't have to!
 
           * You should get a ${random_pet.pet.id} for a pet and name it ${var.favourite_author["first_name"]} ${var.first_teacher_last_name} von ${var.street_name}.
@@ -8,4 +8,8 @@ value = <<EOT
           * Have ${random_shuffle.icecream_choice.result[0]} iceream for dinner.
           * Hahaha. Not gonna be tricked to share your secret.
           EOT
+}
+
+output "version-indicator" {
+  value = "1"
 }
